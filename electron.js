8c1eaafb2996bac14eafb000726ee98b972b87ec
MK2k <gitlab@mk2k.net>
2019-09-11 09:16:42 +0200

    initial commit

---
+++ electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow () {
+    win = new BrowserWindow({ width: 800, height: 600 })
+
+    win.loadFile('dist/index.html')
+
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
+export const eventBus = new Vue({
+});
+
+
+
 new Vue({
-  render: h => h(App),
+	vuetify: new Vuetify({ iconfont: 'mdi' }),
+	render: h => h(App),
 }).$mount('#app')
