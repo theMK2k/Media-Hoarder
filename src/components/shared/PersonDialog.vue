5e0196707401d7cb737a62d2a4184a1e84ec98d0
MK2k <git@mk2k.net>
2019-12-29 21:37:22 +0100

    PersonDialog: bugfix photo URL

--- src/components/shared/PersonDialog.vue
+++ src/components/shared/PersonDialog.vue
@@ -131,7 +131,7 @@ export default {
         this.personData = {
           IMDB_Person_ID: personData.$IMDB_Person_ID,
           Photo_URL: personData.$Photo_URL
-            ? helpers.getPath(personData.$Photo_URL)
+            ? 'file://' + helpers.getPath(personData.$Photo_URL)
             : personData.$Photo_URL,
           ShortBio: personData.$ShortBio,
           LongBio: personData.$LongBio
@@ -163,7 +163,7 @@ export default {
       personData = personData[0];
 
       personData.Photo_URL = personData.Photo_URL
-        ? helpers.getPath(personData.Photo_URL)
+        ? 'file://' + helpers.getPath(personData.Photo_URL)
         : personData.Photo_URL;
 
       this.personData = personData;
