7d28703d62f7d33e81f9bc1b3211480578ac2eeb
MK2k <gitlab@mk2k.net>
2020-09-02 19:04:50 +0200

    shipping of mediainfo removed

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = {
       builderOptions: {
         extraResources: [
           'data/media-hoarder.db_initial',
-          'data/mediainfo/**/*',
           'data/easylist.txt'
         ],
         publish: [
