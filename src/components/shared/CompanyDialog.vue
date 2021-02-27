531354dfddbcc9b0fe82cb12f89d8d35bf0b5852
MK2k <gitlab@mk2k.net>
2021-02-27 17:11:23 +0100

    CompanyDialog: list movies

--- src/components/shared/CompanyDialog.vue
+++ src/components/shared/CompanyDialog.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-dialog v-model="show" persistent max-width="1000px" v-on:keydown.escape="onEscapePressed">
+  <v-dialog
+    v-model="show"
+    persistent
+    max-width="1000px"
+    v-on:keydown.escape="onEscapePressed"
+  >
     <v-card dark flat v-bind:ripple="false">
       <v-list-item style="padding-left: 0px">
         <!-- <div>
@@ -10,13 +15,14 @@
           class="align-self-start"
           style="padding-left: 8px; padding-bottom: 6px"
         >
-          <v-col style="padding: 0px!important" sm="12">
+          <v-col style="padding: 0px !important" sm="12">
             <v-row>
               <div style="margin-left: 16px">
                 <v-list-item-title
                   class="headline mb-2"
-                  style="margin-bottom: 0px!important"
-                >{{$t('Company')}}: {{ Company_Name }}</v-list-item-title>
+                  style="margin-bottom: 0px !important"
+                  >{{ $t("Company") }}: {{ Company_Name }}</v-list-item-title
+                >
               </div>
             </v-row>
 
@@ -27,6 +33,35 @@
               rounded
               height="3"
             ></v-progress-linear>
+
+            <div class="mk-clickable" v-on:click.stop="toggleShowMovies()">
+              <v-row
+                v-if="!isScraping"
+                style="margin-left: 4px; margin-right: 6px; margin-bottom: 8px"
+              >
+                {{
+                  numMovies +
+                  " " +
+                  $t(numMovies === 1 ? "movie" : "movies") +
+                  (!showMovies ? " »" : "")
+                }}
+              </v-row>
+              <div v-if="!isScraping && showMovies">
+                <div v-for="(movie, index) in movies" v-bind:key="index">
+                  <v-row
+                    style="
+                      margin-left: 20px;
+                      margin-right: 6px;
+                      margin-bottom: 8px;
+                    "
+                  >
+                    {{ movie.Name }}
+                    {{ movie.Name2 ? " | " + movie.Name2 : "" }}
+                    {{ movie.yearDisplay }}
+                  </v-row>
+                </div>
+              </div>
+            </div>
           </v-col>
         </v-list-item-content>
       </v-list-item>
@@ -36,13 +71,14 @@
             class="xs-fullwidth"
             color="secondary"
             v-on:click.native="onCloseClick"
-            style="margin-left: 8px;"
-          >{{$t('Close')}}</v-btn>
+            style="margin-left: 8px"
+            >{{ $t("Close") }}</v-btn
+          >
           <v-btn
             class="xs-fullwidth"
             color="primary"
             v-on:click.stop="openIMDB()"
-            style="margin-left: 8px;"
+            style="margin-left: 8px"
           >
             <v-icon small>mdi-web</v-icon>&nbsp;IMDB
           </v-btn>
@@ -50,10 +86,10 @@
             class="xs-fullwidth"
             color="primary"
             v-on:click.native="onFilterClick"
-            style="margin-left: 8px;"
+            style="margin-left: 8px"
           >
-            {{$t('Filter by this company')}}
-            <span v-if="numMovies">({{numMovies}})</span>
+            {{ $t("Filter by this company") }}
+            <span v-if="numMovies">({{ numMovies }})</span>
           </v-btn>
         </v-row>
       </v-col>
@@ -76,24 +112,29 @@ export default {
   data() {
     return {
       isScraping: false,
-      numMovies: null
+      numMovies: null,
+      movies: [],
+      showMovies: false,
     };
   },
 
   watch: {
-    IMDB_Company_ID: function(newVal) {
+    IMDB_Company_ID: function (newVal) {
       this.init(newVal);
-    }
+    },
   },
 
   methods: {
     async init($IMDB_Company_ID) {
+      this.movies = [];
+      this.showMovies = false;
+
       this.numMovies = await store.db.fireProcedureReturnScalar(
         `
         SELECT COUNT(1) FROM
         (
           SELECT DISTINCT
-            MC.id_Movies
+              MOV.Name || ' ' || IFNULL(MOV.startYear, 'xxx')
           FROM tbl_Movies_IMDB_Companies MC
         	INNER JOIN tbl_Movies MOV ON MC.id_Movies = MOV.id_Movies
           WHERE MC.IMDB_Company_ID = $IMDB_Company_ID
@@ -107,7 +148,7 @@ export default {
     onButtonClick(eventName) {
       this.$emit(eventName, {
         dontAskAgain: this.dontAskAgainValue,
-        textValue: this.textValueLocal
+        textValue: this.textValueLocal,
       });
 
       this.resetData();
@@ -138,7 +179,7 @@ export default {
       await store.addFilterCompany(this.Company_Name);
 
       const setFilter = {
-        filterCompanies: [this.Company_Name]
+        filterCompanies: [this.Company_Name],
       };
 
       eventBus.companyDialogConfirm(setFilter);
@@ -156,11 +197,77 @@ export default {
 
     onEscapePressed() {
       this.onCloseClick();
-    }
+    },
+
+    async toggleShowMovies() {
+      if (this.showMovies) {
+        this.showMovies = false;
+        return;
+      }
+
+      if (!this.movies.length > 0) {
+        this.isLoadingMovies = true;
+        const movies = (
+          await store.fetchMedia("movies", null, true, this.$t, {
+            filterSettings: {},
+            filterCompanies: [
+              {
+                id_Filter_Companies: 0,
+                Selected: false,
+              },
+              {
+                id_Filter_Companies: 666,
+                Selected: true,
+                IMDB_Company_ID: this.IMDB_Company_ID,
+                Company_Name: this.Company_Name
+              },
+            ],
+          })
+        )
+          .sort((a, b) => {
+            if (a.startYear > b.startYear) {
+              return -1;
+            }
+            if (a.startYear < b.startYear) {
+              return 1;
+            }
+            if (a.Name.toLowerCase() < b.Name.toLowerCase()) {
+              return -1;
+            }
+            if (a.Name.toLowerCase() > b.Name.toLowerCase()) {
+              return 1;
+            }
+
+            return 0;
+          })
+          .map((item) => {
+            return {
+              Name: item.Name,
+              Name2: item.Name2,
+              yearDisplay: item.yearDisplay,
+            };
+          });
+
+        this.movies = movies.filter((item, index) => {
+          return (
+            movies.findIndex((item2) => {
+              return (
+                `${item2.Name} ${item2.yearDisplay}` ===
+                `${item.Name} ${item.yearDisplay}`
+              );
+            }) === index
+          );
+        });
+
+        this.isLoadingMovies = false;
+      }
+
+      this.showMovies = true;
+    },
   },
 
   // ### Lifecycle Hooks ###
-  created() {}
+  created() {},
 };
 </script>
 
