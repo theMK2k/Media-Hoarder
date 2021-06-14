74da3117e63a3fb08a1b32e94093c5d3e9281ef3
MK2k <gitlab@mk2k.net>
2021-06-14 11:58:14 +0200

    i18n key checks

--- src/i18n.js
+++ src/i18n.js
@@ -9,6 +9,7 @@ Vue.use(VueI18n)
 
 function loadLocaleMessages () {
   const locales = require.context('./i18n', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+
   const messages = {}
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
@@ -17,12 +18,15 @@ function loadLocaleMessages () {
       messages[locale] = locales(key)
     }
   })
+
+  logger.log('locales messages:', messages);
+
   return messages
 }
 
 const messages = loadLocaleMessages();
 
-logger.log('locale messages:', messages);
+validateMessages(messages);
 
 // Load Moment Locales
 Object.keys(messages).forEach(key => {
@@ -30,6 +34,42 @@ Object.keys(messages).forEach(key => {
     moment.locale(key, messages[key].moment);
   }
 })
+
+// any other locale should have at least the keys, that "en" has
+function validateMessages(messages) {
+  Object.keys(messages).forEach(locale => {
+    if (locale === 'en') {
+      return;
+    }
+    
+    const checkKeys = function (en, other) {
+      Object.keys(en).forEach(key => {
+        if (typeof en[key] === 'string') {
+          if (en[key].includes('_')) {
+            logger.warn(`Locale en contains underscore in key '${key}'`);
+          }
+        }
+        
+        if (other[key] === undefined) {
+          logger.warn(`Locale ${locale} is missing key '${key}'!`);
+          return;
+        }
+
+        if (typeof en[key] === 'object') {
+          checkKeys(en[key], other[key]);
+        }
+        
+        if (typeof other[key] === 'string') {
+          if (other[key].includes('_')) {
+            logger.warn(`Locale ${locale} contains underscore in key '${key}'`);
+          }
+        }
+      })
+    }
+
+    checkKeys(messages.en, messages[locale]);
+  })
+}
 
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
