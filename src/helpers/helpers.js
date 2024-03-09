49a322203528411afbabd3277387c99acf0a2a83
MK2k <gitlab@mk2k.net>
2024-03-09 10:38:20 +0100

    update user agent to firefox v123.0

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -327,7 +327,7 @@ async function requestAsync(options) {
   }
 
   optionsDerived.headers["User-Agent"] =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0";
 
   optionsDerived.timeout = 10000; // we set a 10s timeout
 
