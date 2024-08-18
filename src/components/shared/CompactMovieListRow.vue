98c9a3ae3bbf292dfd7807c95b3863286019bc0a
MK2k <gitlab@mk2k.net>
2024-08-18 22:21:39 +0200

    CompactMovieListRow: fix layout

--- src/components/shared/CompactMovieListRow.vue
+++ src/components/shared/CompactMovieListRow.vue
@@ -5,8 +5,9 @@
     style="padding-top: 4px; padding-bottom: 2px; margin-top: 2px"
     v-on:click="onClick"
   >
-    <div>
-      <div>
+    <div style="display: flex; justify-content: space-between; width: 100%">
+      <!-- Name, Details (left) -->
+      <div id="name-and-details" style="flex-grow: 1">
         <span v-if="movie.Series_Season_Displaytext" style="font-weight: 400; color: lightgray; margin-right: 4px">
           {{ movie.Series_Season_Displaytext
           }}{{ `${movie.Series_Season_Displaytext ? "." : ""}${movie.Series_Episodes_Displaytext}` }}
@@ -15,32 +16,34 @@
         {{ movie.Name2 ? " | " + movie.Name2 : "" }}
         {{ movie.yearDisplay }}
         {{ isClickable && showExpandIndicator && isCollapsed ? " »" : "" }}
-      </div>
-      <div v-if="movie.propertyDetails" class="mk-light-grey" style="margin-top: 2px">
-        <span v-for="(detail, index) in movie.propertyDetails" v-bind:key="index">
-          <span v-if="index > 0"> | </span>
-          <span v-if="detail.Category || detail.CategoryToTranslate">
-            {{ detail.CategoryToTranslate ? $t(detail.CategoryToTranslate) : detail.Category
-            }}<span v-if="detail.Value">: </span>
+
+        <div v-if="movie.propertyDetails" class="mk-light-grey" style="margin-top: 2px">
+          <span v-for="(detail, index) in movie.propertyDetails" v-bind:key="index">
+            <span v-if="index > 0"> | </span>
+            <span v-if="detail.Category || detail.CategoryToTranslate">
+              {{ detail.CategoryToTranslate ? $t(detail.CategoryToTranslate) : detail.Category
+              }}<span v-if="detail.Value">: </span>
+            </span>
+            <span v-if="detail.Value">{{ detail.Value }}</span>
           </span>
-          <span v-if="detail.Value">{{ detail.Value }}</span>
-        </span>
+        </div>
+      </div>
+
+      <!-- Rating (right) -->
+      <div id="rating" style="flex-shrink: 0; margin-left: 16px">
+        <span v-if="movie.IMDB_rating_defaultDisplay">
+          <v-icon small color="amber" style="padding-bottom: 4px; width: 12px; height: 12px">mdi-star</v-icon>
+          {{ movie.IMDB_rating_defaultDisplay }}</span
+        >
+        <span
+          v-if="movie.IMDB_metacriticScore"
+          v-bind:class="helpers.getMetaCriticClass(movie.IMDB_metacriticScore)"
+          class="mk-compact-movie-list-metacritic-block"
+          >{{ movie.IMDB_metacriticScore }}</span
+        >
+        <span v-if="!movie.IMDB_metacriticScore" class="mk-compact-movie-list-metacritic-block"> &nbsp;</span>
       </div>
     </div>
-    <v-spacer />
-    <span>
-      <span v-if="movie.IMDB_rating_defaultDisplay">
-        <v-icon small color="amber" style="padding-bottom: 4px; width: 12px; height: 12px">mdi-star</v-icon>
-        {{ movie.IMDB_rating_defaultDisplay }}</span
-      >
-      <span
-        v-if="movie.IMDB_metacriticScore"
-        v-bind:class="helpers.getMetaCriticClass(movie.IMDB_metacriticScore)"
-        class="mk-compact-movie-list-metacritic-block"
-        >{{ movie.IMDB_metacriticScore }}</span
-      >
-      <span v-if="!movie.IMDB_metacriticScore" class="mk-compact-movie-list-metacritic-block"> &nbsp;</span>
-    </span>
   </v-row>
 </template>
 
